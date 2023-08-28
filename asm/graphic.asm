5a522545c0da8dbb5439955521e60decd0e7edf0
Mathéo Vignaud <64284499+Pokecreatorfr@users.noreply.github.com>
2023-08-28 14:37:21 +0200

    Update graphic.asm

--- asm/graphic.asm
+++ asm/graphic.asm
@@ -1,15 +1,2 @@
-.section rodata
-
-.global tileset1
-.align 4
-
-tileset1:
-.incbin "graphics/tileset1.png"
-
-.global tileset1_size
-.align 4
-
-tileset1_size:
-.int 0x00000000 + tileset1_size - tileset1
 
 
